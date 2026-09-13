d8cf607be9fe229ba31efde0d04071ec5022d1cc
esmaelmakkii-droid <esmaelmakkii@gmail.com>
2026-09-13 23:16:09 +0200

    Create supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,7 @@
+// سكرتيري - إعداد Supabase
+// المطوران: إسماعيل بكري وعبير عبد الرحيم
+
+window.SEKRETARY_CLOUD = window.SEKRETARY_CLOUD || {
+  url: "https://svdbptpqmcziigbancss.supabase.co",
+  anonKey: "sb_publishable_GBVOpmJ7pFe_olxzLuK0xA_VUlqWX3w"
+};
